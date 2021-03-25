899ce1fcc8657b7f9fd137db96d7e863582aee2e
20-ist3AlexseyEremin <78901825+20-ist3AlexseyEremin@users.noreply.github.com>
2021-03-25 12:49:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,18 +2,18 @@
 
 int cbinsearch(int *arr, int size, int value) {
         int i = 0;
-        int j = size - 1;
+        j = size - 1;
         int k = 0;
         while (i < j) {
                 int center = (i + j) / 2;
                 if (arr[center] < value)
-                        i = center + 1;
+                        i = center+1;
                 else
                         j = center;
                 if (arr[i] == value) {
                         while (arr[i] == value) {
                                 k++;
-                                j++;
+                                i++;
                         }
                 }
         }
